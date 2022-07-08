918d0e276fcbc522b6611a8400e99c7629b15530
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-07-08 19:42:22 -0300

    Add files via upload

--- package_basesCursoR_imdb.R
+++ package_basesCursoR_imdb.R
@@ -24,13 +24,15 @@ View(imdb)
 # Selecionar dados -------------------------------------------------------------------------------------------------------------------------
 
 imdb1 <- imdb %>%
-  select(genero, pais, duracao, nota_imdb) %>%
+  select(genero, pais, duracao, nota_imdb, ano) %>%
+  filter(ano %in% c("1906", "1914", "1949", "1976", "1980", "2004")) %>%
   filter(genero %in% c("Romance", "Drama", "Comedy", "Horror", "Western")) %>%
   filter(pais %in% c("USA", "France", "Russia", "Germany", "Spanish"))
 View(imdb1)  
 glimpse(imdb1)
 imdb1$genero <- as.factor(imdb1$genero)
 imdb1$pais <- as.factor(imdb1$pais)
+imdb1$ano <- as.factor(imdb1$ano)
 
 # Análises ---------------------------------------------------------------------------------------------------------------------------------
 
@@ -82,6 +84,34 @@ imdb5 <- imdb1 %>%
             sd = sd(duracao),n = n(),
             se = sd/sqrt(n))
 View(imdb5)
+
+p4 <- ggplot(imdb5, aes(x = fct_reorder(pais, med), y = med)) +
+  geom_col(fill = "#7fc97f", color = "black") +
+  geom_errorbar(aes(x = pais, y = med, ymin = med - se,
+                    ymax = med + se), width = 0.2, size = 0.9) +
+  labs(x = "Países", y = "Duração dos filmes")
+p4
+
+imdb5 <- imdb1 %>%
+  group_by(ano) %>%
+  summarise(med = mean(nota_imdb),
+            sd = sd(nota_imdb),n = n(),
+            se = sd/sqrt(n))
+View(imdb5)
+
+p5 <- ggplot(imdb4, aes(x = fct_reorder(pais, med), y = med)) +
+  geom_col(fill = "#7fc97f", color = "black") +
+  geom_errorbar(aes(x = pais, y = med, ymin = med - se,
+                    ymax = med + se), width = 0.2, size = 0.9) +
+  labs(x = "Países", y = "Notas IMDB")
+p5
+
+imdb6 <- imdb1 %>%
+  group_by(ano) %>%
+  summarise(med = mean(duracao),
+            sd = sd(duracao),n = n(),
+            se = sd/sqrt(n))
+View(imdb6)
 
 p4 <- ggplot(imdb5, aes(x = fct_reorder(pais, med), y = med)) +
   geom_col(fill = "#7fc97f", color = "black") +
